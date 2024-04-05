03aa680a16646727aed8f5e22befbd3ed566732b
Brandon Willetts <brandonwilletts@gmail.com>
2024-04-05 15:00:36 -0700

    Declare variables inside functions
    Declare playerSelection and computerSelection inside of playRound to
    negate need for function parameters.
    
    Changed function returns to console.log

--- javascript.js
+++ javascript.js
@@ -15,41 +15,41 @@ function getComputerChoice() {
     return computerSelection;
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound () {
+    let playerSelection = prompt("Enter 'rock', 'paper', or 'scissors'").toLowerCase();
+    let computerSelection = getComputerChoice();
     if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore++
-        return "You win! Rock beats Scissors - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")";
+        console.log("You win! Rock beats Scissors - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")");
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         computerScore++
-        return "You lose! Paper beats Rock - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")";
+        console.log("You lose! Paper beats Rock - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")");
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         computerScore++
-        return "You lose! Rock beats Scissors - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")";
+        console.log("You lose! Rock beats Scissors - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")");
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerScore++
-        return "You win! Scissors beats Paper - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")";
+        console.log("You win! Scissors beats Paper - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")");
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         computerScore++
-        return "You lose! Scissors beats Paper - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")";
+        console.log("You lose! Scissors beats Paper - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")");
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore++
-        return "You win! Paper beats Rock - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")";
+        console.log("You win! Paper beats Rock - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")");
     } else {
-        return "Tie! Nobody wins - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")";
+        console.log("Tie! Nobody wins - Current Score: You (" + playerScore + ") - Computer (" + computerScore + ")");
     }
 }
 
 function playGame () {
     for (let i = 0; i < 5; i++){
-        const playerSelection = prompt("Enter 'rock', 'paper', or 'scissors'").toLowerCase();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+        playRound();
         if (i === 4 && (playerScore > computerScore)) {
-            return "You win the game! Score: You (" + playerScore + ") - Computer (" + computerScore + ")";
+            console.log("You win the game! Score: You (" + playerScore + ") - Computer (" + computerScore + ")");
         } else if (i === 4 && (playerScore < computerScore)) {
-            return "You lose the game! Score: You (" + playerScore + ") - Computer (" + computerScore + ")";
+            console.log("You lose the game! Score: You (" + playerScore + ") - Computer (" + computerScore + ")");
         } else if (i === 4 && (playerScore === computerScore)) {
-            return "You tied the game! Score: You (" + playerScore + ") - Computer (" + computerScore + ")";
+            console.log("You tied the game! Score: You (" + playerScore + ") - Computer (" + computerScore + ")");
         }
     }
 }
@@ -57,4 +57,4 @@ function playGame () {
 let playerScore = 0;
 let computerScore = 0;
 
-console.log(playGame());
+playGame();
