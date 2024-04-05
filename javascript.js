4f16fdb97a6c285675018c134ab125396292fb3c
Brandon Willetts <brandonwilletts@gmail.com>
2024-04-05 12:45:22 -0700

    Complete pseudocode to breakdown the assignment

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-console.log("hello");
+//PSEUDOCODE
+
+//Generate random "rock", "paper", "scissors" selection (getComputerChoice)
+//Take in prompt from player (playerSelection)
+    //Check that playerSelection is valid (case insensitive)
+        //If so, play game
+        //If not, return error message and request new playerSelection
+//Keep track of wins for player and computer
+//Compare playerSelection and computerSelection and determine who wins (playGame)
+    //If player wins, add 1 to playerScore, add 1 to gamesPlayed, display message and play again
+    //If computer wins, add 1 to computerScore, add 1 to gamesPlayed, display message and play again
+    //If tie, just display message and play again
+//Check # of games played (playRound)
+    //If < 5, keep playing
+    //If === 5, compare playerWins to computerWins, return the winner, and display message
+
+
+
+
+
